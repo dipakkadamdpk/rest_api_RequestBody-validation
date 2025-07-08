b9f67571eb069e10711c6c5783939703e2187d1a
Dipak <dipakkadamdpk@gmail.com>
2025-07-08 20:06:06 +0530

    updated concept  file

---
+++ rest_api_RequestBody_validation/src/main/java/com/springboot/mvc/Concept.java
@@ -0,0 +1,16 @@
+package com.springboot.mvc;
+
+public class Concept {
+	/*
+	 
+	 1. Add validation dependency into the POM.xml
+	 
+	 2. Add required annotation for RequestDTO classes.
+	 
+	 3. check same in the Swagger UI.
+	 
+	 
+	 
+	 */
+
+}
